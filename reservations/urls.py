a582e49ad69ff53534f3a114fcf2ed18e5258719
a-rod85 <31aaronanderson@gmail.com>
2023-02-16 09:30:32 +0000

    urls.py res added to

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import reservation_list, reservation_detail, reservation_create, reservation_update, reservation_delete
-
+from .views import reservation_list, reservation_detail, reservation_create
+from .views import reservation_update, reservation_delete
 urlpatterns = [
     path('', reservation_list, name='reservation_list'),
     path('<int:pk>/', reservation_detail, name='reservation_detail'),
