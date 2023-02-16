8c33f6fb65f7757c9f72e15a56e2eda2b0fdd816
a-rod85 <31aaronanderson@gmail.com>
2023-02-16 09:02:33 +0000

    create urls.py, create forms.py

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    date = models.DateField()
+    time = models.TimeField()
+    num_of_guests = models.IntegerField()
--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from .models import Reservation
 
-# Create your views here.
+
+def reservation_list(request):
+    reservations = Reservation.objects.all()
+    return render(request, 'reservation_list.html', {'reservations': reservations})
+
+
+def reservation_detail(request, pk):
+    reservation = Reservation.objects.get(pk=pk)
+    return render(request, 'reservation_detail.html', {'reservation': reservation})
+
+
+def reservation_create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone_number = request.POST.get('phone_number')
+        date = request.POST.get('date')
+        time = request.POST.get('time')
+        num_of_guests = request.POST.get('num_of_guests')
+        Reservation.objects.create(name=name, email=email, phone_number=phone_number,
+        date=date, time=time, num_of_guests=num_of_guests)
+        return redirect('reservation_list')
+    return render(request, 'reservation_create.html')
+
+
+def reservation_update(request, pk):
+    reservation = Reservation.objects.get(pk=pk)
+    if request.method == 'POST':
+        reservation.name = request.POST.get('name')
+        reservation.email = request.POST.get('email')
+        reservation.phone_number = request.POST.get('phone_number')
+        reservation.date = request.POST.get('date')
+        reservation.time = request.POST.get('time')
+        reservation.num_of_guests = request.POST.get('num_of_guests')
+        reservation.save()
+        return redirect('reservation_list')
+    return render(request, 'reservation_update.html', {'reservation': reservation})
+
+
+def reservation_delete(request, pk):
+    reservation = Reservation.objects.get(pk=pk)
+    reservation.delete()
+    return redirect('reservation_list')
